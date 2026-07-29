433e09441abe6f7e1fd984070d0cdf0ab64826f9
shofiq18 <shafiqul.islam25021998@gmail.com>
2026-07-29 20:59:38 +0600

    update  corosual section

--- redux/api/baseApi.ts
+++ redux/api/baseApi.ts
@@ -1,23 +1,23 @@
-
-
-
-
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import Cookies from "js-cookie";
 
-export const baseApi = createApi({
-  reducerPath: "baseApi",
-  baseQuery: fetchBaseQuery({
-    baseUrl: `${process.env.NEXT_PUBLIC_API_URL}`,
-    credentials: "include",
-    prepareHeaders: (headers) => {
+const rawBaseQuery = fetchBaseQuery({
+  baseUrl: `${process.env.NEXT_PUBLIC_API_URL || ""}`,
+  credentials: "include",
+  prepareHeaders: (headers) => {
+    if (typeof window !== "undefined") {
       const accessToken = Cookies.get("accessToken");
       if (accessToken) {
         headers.set("Authorization", accessToken);
       }
-      return headers;
-    },
-  }),
+    }
+    return headers;
+  },
+});
+
+export const baseApi = createApi({
+  reducerPath: "baseApi",
+  baseQuery: rawBaseQuery,
   endpoints: () => ({}),
   tagTypes: [
     "User",
